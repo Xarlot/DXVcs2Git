4801d8c63e1352e2201cbec515dfd91b61c1488b
Xarlot <maximatorr@bk.ru>
2015-09-09 21:17:34 +0300

    add dxvcs

---
+++ DXVcs2Git.DXVcs/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXVcs2Git.DXVcs
+{
+    public class Class1
+    {
+    }
+}
