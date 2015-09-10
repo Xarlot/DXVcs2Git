320e675021572340225c851b6d7767683929d519
Xarlot <maximatorr@bk.ru>
2015-09-10 13:11:55 +0300

    add service.getproject

--- DXVcs2Git.DXVcs/IDXVcsRepository.cs
+++ DXVcs2Git.DXVcs/IDXVcsRepository.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using DXVCS;
 
 namespace DXVcs2Git.DXVcs {
     public interface IDXVcsRepository {
-        ProjectHistoryInfo[] GetProjectHistory(string vcsFile, bool resursive, DateTime? from = null, DateTime? to = null);
+        IList<ProjectHistoryInfo> GetProjectHistory(string vcsFile, bool resursive, DateTime? from = null, DateTime? to = null);
         FileVersionInfo[] GetFileHistory(string vcsFile, out string fileName);
         FileDiffInfo GetFileDiffInfo(string vcsFile, SpacesAction spacesAction = SpacesAction.IgnoreAll);
         FileDiffInfo GetFileDiffInfo(string vcsFile, Action<int, int> progressAction, SpacesAction spacesAction);
-        void GetLatestVersion(string vcsFile, string fileName);
+        void GetProject(string vcsPath, string localPath, DateTime timeStamp);
+        void GetLatestFileVersion(string vcsFile, string fileName);
         void Get(string vcsFile, string fileName, int version);
         void CheckOutFile(string vcsFile, string localFile, string comment);
         void CheckInFile(string vcsFile, string localFile, string comment);
--- DXVcs2Git.Tests/DXVcsServiceTests.cs
+++ DXVcs2Git.Tests/DXVcsServiceTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using DXVcs2Git.DXVcs;
 using DXVCS;
 using NUnit.Framework;
@@ -18,7 +21,7 @@ namespace DXVcs2Git.Tests {
         public void GetProjectHistoryFromTestHistory() {
             var repo = DXVcsConectionHelper.Connect(defaultConfig.AuxPath);
             var history = repo.GetProjectHistory(@"$/Sandbox/litvinov/DXVcsTest/testhistory", true, new DateTime(2015, 9, 9), new DateTime(2015, 9, 10));
-            Assert.AreEqual(3, history.Length);
+            Assert.AreEqual(3, history.Count());
             Assert.AreEqual(@"9/9/2015 7:30:57 PM,,,Create,,Project,Litvinov,1", FormatProjectHistoryItem(history[0]));
             Assert.AreEqual(@"9/9/2015 7:31:09 PM,,,Shared from $/Sandbox/litvinov/DXVcsTest/test.txt,test.txt,File,Litvinov,2", FormatProjectHistoryItem(history[1]));
             Assert.AreEqual(@"9/9/2015 7:05:37 PM,,,Checked in (2),test.txt,File,Litvinov,2", FormatProjectHistoryItem(history[2]));
@@ -45,10 +48,31 @@ namespace DXVcs2Git.Tests {
             Assert.AreEqual(@"9/9/2015 7:47:22 PM,1,,Checked in (2),1.txt,File,Litvinov,2", FormatProjectHistoryItem(grouped[3].ToList()[0]));
             Assert.AreEqual(@"9/9/2015 7:47:22 PM,1,,Checked in (2),2.txt,File,Litvinov,2", FormatProjectHistoryItem(grouped[3].ToList()[1]));
         }
-        [Test, Explicit]
-        public void GetProjectHistoryForXpf152() {
+        [Test]
+        public void GetProjectForTimeStamp() {
             var repo = DXVcsConectionHelper.Connect(defaultConfig.AuxPath);
-            var history = repo.GetProjectHistory(@"$/2015.2", true);
+        }
+        [Test, Explicit]
+        public void GetProjectHistoryForXpfCore152() {
+            var repo = DXVcsConectionHelper.Connect(defaultConfig.AuxPath);
+            var vcsPath81 = @"$/NET.OLD/2008.1/WPF/DevExpress.Wpf.Core";
+            //var vcsPath = @"$/2015.2/XPF/DevExpress.Xpf.Core/DevExpress.Xpf.Core";
+            var history = repo.GetProjectHistory(vcsPath81, true);
+            var grouped = history.Reverse().GroupBy(x => x.ActionDate).OrderBy(x => x.First().ActionDate).Select(x => new HistoryItem(x.First().ActionDate, x.ToList()));
+        }
+
+        [Test]
+        void UpdateProjectForGroupedHistory() {
+            
+        }
+    }
+    class HistoryItem {
+        public DateTime TimeStamp { get; private set; }
+        public IList<ProjectHistoryInfo> History { get; private set; }
+
+        public HistoryItem(DateTime timeStamp, IList<ProjectHistoryInfo> info) {
+            TimeStamp = timeStamp;
+            History = info;
         }
     }
 }
