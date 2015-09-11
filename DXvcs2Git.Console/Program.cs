ee830c2825fe58e9f8a8874a8c54b5eb961a4bdb
Xarlot <maximatorr@bk.ru>
2015-09-11 14:24:55 +0300

    add generating

--- DXvcs2Git.Console/Program.cs
+++ DXvcs2Git.Console/Program.cs
@@ -11,7 +11,7 @@ using LibGit2Sharp;
 namespace DXvcs2Git.Console {
     class Program {
         static Credentials credentials;
-        static string path = @"c:\test\";
+        static string path = @"z:\test\";
         static string testUrl = "http://litvinov-lnx/tester/testxpfall.git";
         static void Main(string[] args) {
 
@@ -19,23 +19,23 @@ namespace DXvcs2Git.Console {
             List<string> branches = new List<string>() {
                 @"$/NET.OLD/2010.1/XPF/DevExpress.Xpf.Core",
                 @"$/NET.OLD/2010.2/XPF/DevExpress.Xpf.Core",
-                //@"$/NET.OLD/2011.1/XPF/DevExpress.Xpf.Core",
-                //@"$/NET.OLD/2011.2/XPF/DevExpress.Xpf.Core",
-                //@"$/NET.OLD/2012.1/XPF/DevExpress.Xpf.Core",
-                //@"$/NET.OLD/2012.2/XPF/DevExpress.Xpf.Core",
-                //@"$/NET.OLD/2013.1/XPF/DevExpress.Xpf.Core",
-                //@"$/NET.OLD/2013.2/XPF/DevExpress.Xpf.Core",
-                //@"$/2014.1/XPF/DevExpress.Xpf.Core",
-                //@"$/2014.2/XPF/DevExpress.Xpf.Core",
-                //@"$/2015.1/XPF/DevExpress.Xpf.Core",
-                //@"$/2015.2/XPF/DevExpress.Xpf.Core",
+                @"$/NET.OLD/2011.1/XPF/DevExpress.Xpf.Core",
+                @"$/NET.OLD/2011.2/XPF/DevExpress.Xpf.Core",
+                @"$/NET.OLD/2012.1/XPF/DevExpress.Xpf.Core",
+                @"$/NET.OLD/2012.2/XPF/DevExpress.Xpf.Core",
+                @"$/NET.OLD/2013.1/XPF/DevExpress.Xpf.Core",
+                @"$/NET.OLD/2013.2/XPF/DevExpress.Xpf.Core",
+                @"$/2014.1/XPF/DevExpress.Xpf.Core",
+                @"$/2014.2/XPF/DevExpress.Xpf.Core",
+                @"$/2015.1/XPF/DevExpress.Xpf.Core",
+                @"$/2015.2/XPF/DevExpress.Xpf.Core",
             };
 
             System.Console.WriteLine($"========   Start generating branch timings   ===========");
             List<DateTime> branchesCreatedTime = branches.Select(x => {
                 var history = repo.GetProjectHistory(x, true);
                 return history.First(IsBranchCreatedTimeStamp).ActionDate;
-            }).Concat(new[] { DateTime.Now}).ToList();
+            }).Concat(new[] { DateTime.Now }).ToList();
             System.Console.WriteLine($"========   Completed generating branch timings   ===========");
             System.Console.WriteLine($"========   Start generating project history   ===========");
             var resultHistory = Enumerable.Empty<HistoryItem>();
@@ -56,7 +56,6 @@ namespace DXvcs2Git.Console {
             }
             resultHistory = resultHistory.ToList();
             System.Console.WriteLine($"========   Completed generating project history   ===========");
-
             InitUserCredentials();
             string gitPath = InitGit(path);
             using (var gitRepo = new Repository(gitPath)) {
@@ -68,14 +67,18 @@ namespace DXvcs2Git.Console {
                 System.Console.WriteLine($"========   Startup git fetch completed   ===========");
                 foreach (var item in resultHistory) {
                     System.Console.WriteLine($"========   Start updating project {item.Branch} {item.TimeStamp} ===========");
+                    CleanUpDir(path);
                     repo.GetProject(item.Branch, path, item.TimeStamp);
                     System.Console.WriteLine($"========   Completed updating project   ===========");
                     if (IsDirEmpty(path)) {
                         System.Console.WriteLine($"========   No history for {item.Branch}  {item.TimeStamp}  ===========");
                         continue;
                     }
+                    PreprocessRepo(path);
 
                     gitRepo.Fetch(network.Name, fetchOptions);
+                    gitRepo.Stage("*");
+
                     string user = item.History.First().User;
                     System.Console.WriteLine($"========   Start git commit {user} {item.TimeStamp}  ===========");
                     CommitOptions commitOptions = new CommitOptions();
@@ -89,6 +92,31 @@ namespace DXvcs2Git.Console {
                     gitRepo.Network.Push(gitRepo.Branches["master"], pushOptions);
                     System.Console.WriteLine($"========   Completed git push  ===========");
                 }
+            }
+        }
+        static void CleanUpDir(string path) {
+            string gitPath = Path.Combine(path, ".git");
+            foreach (var dir in Directory.EnumerateDirectories(path)) {
+                if (dir == gitPath)
+                    continue;
+                Directory.Delete(dir, true);
+                foreach (var file in Directory.EnumerateFiles(path)) {
+                    File.Delete(file);
+                }
+            }
+        }
+        static void PreprocessRepo(string path) {
+            string gitPath = Path.Combine(path, ".git");
+            foreach (var dir in Directory.EnumerateDirectories(path)) {
+                if (dir == gitPath)
+                    continue;
+                PreprocessRepo(dir);
+                if (!Directory.EnumerateFiles(dir).Any())
+                    AddEmptyGitIgnore(dir);
+            }
+        }
+        static void AddEmptyGitIgnore(string path) {
+            using (var file = File.Create(Path.Combine(path, ".gitignore"))) {
             }
         }
         static bool IsDirEmpty(string path) {
