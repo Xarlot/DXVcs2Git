bcde554ffe77a5480007036197030cea947c65cd
Xarlot <maximatorr@bk.ru>
2015-09-15 07:16:55 +0300

    add release config

---
+++ DXvcs2Git.Console/cleanup.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set "excludeFile= cleanup.bat DXVcs2Git.Console.exe.config DXVcs2Git.UI.exe.config "
+set "excludeExt= .bat .dll .exe .txt "
+
+for %%v in (*.*) do (
+   if "!excludeFile: %%v =!" equ "%excludeFile%" if "!excludeExt: %%~Xv =!" equ "%excludeExt%" del %%v
+)
