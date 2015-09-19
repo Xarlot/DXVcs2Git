c7b0f290b3588d70fc6d46a3e7f3efde96cf145c
Xarlot <maximatorr@bk.ru>
2015-09-19 10:15:40 +0300

    get merge requests from gitlab

---
+++ DXVcs2Git.Tests/GitlabWrapperTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using DXVcs2Git.Git;
+using NUnit.Framework;
+
+namespace DXVcs2Git.Tests {
+    [TestFixture]
+    public class GitlabWrapperTests {
+        const string token = "X6XV2G_ycz_U4pi4m93K";
+
+        [Test]
+        public void GetProject() {
+            GitLabWrapper wrapper = new GitLabWrapper("http://litvinov-lnx", token);
+            var project = wrapper.FindProject("tester/testxpfall");
+            Assert.IsNotNull(project);
+        }
+        [Test]
+        public void GetMergeRequests() {
+            GitLabWrapper wrapper = new GitLabWrapper("http://litvinov-lnx", token);
+            var project = wrapper.FindProject("tester/testxpfall");
+            var requests = wrapper.GetMergeRequests(project);
+        }
+    }
+}
---
+++ DXVcs2Git/GitLabWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NGitLab;
+using NGitLab.Models;
+
+namespace DXVcs2Git.Git {
+    public class GitLabWrapper {
+        readonly GitLabClient client;
+        public GitLabWrapper(string server, string token) {
+            client = GitLabClient.Connect(server, token);
+        }
+        public Project FindProject(string project) {
+            return client.Projects.Accessible.FirstOrDefault(x => x.PathWithNamespace == project);
+        }
+        public IEnumerable<MergeRequest> GetMergeRequests(Project project) {
+            var mergeRequestsClient = client.GetMergeRequest(project.Id);
+            return mergeRequestsClient.All;
+        }
+    }
+}
