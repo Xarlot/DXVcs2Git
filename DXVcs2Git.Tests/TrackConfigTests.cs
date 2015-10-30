2b02c9b26a08cb205c77ab5a0c9e847585beee6d
Litvinov <maxim.litvinov@devexpress.com>
2015-10-30 15:48:22 +0300

    generate config

--- DXVcs2Git.Tests/TrackConfigTests.cs
+++ DXVcs2Git.Tests/TrackConfigTests.cs
@@ -19,22 +19,34 @@ namespace DXVcs2Git.Tests {
             serializer.Serialize(new List<TrackBranch>() { branch }, @"c:\1\trackconfig_testxpf.config");
         }
         [Test, Explicit]
-        public void GenerateXpfCommonConfig() {
+        public void GenerateXpfCommon152Config() {
+            GenerateXpfCommonCongfig("2015.2");
+        }
+        [Test, Explicit]
+        public void GenerateXpfCommon151Config() {
+            GenerateXpfCommonCongfig("2015.1");
+        }
+        [Test, Explicit]
+        public void GenerateXpfCommon142Config() {
+            GenerateXpfCommonCongfig("2014.2");
+        }
+        void GenerateXpfCommonCongfig(string branchName) {
             List<TrackItem> items = new List<TrackItem>();
-            items.Add(new TrackItem() { Path = @"$/2015.2/XPF/DevExpress.Mvvm", ProjectPath = "DevExpress.Mvvm" });
-            items.Add(new TrackItem() { Path = @"$/2015.2/XPF/DevExpress.Xpf.Core", ProjectPath = "DevExpress.Xpf.Core" });
-            items.Add(new TrackItem() { Path = @"$/2015.2/XPF/DevExpress.Xpf.Controls", ProjectPath = "DevExpress.Xpf.Controls" });
-            items.Add(new TrackItem() { Path = @"$/2015.2/XPF/DevExpress.Xpf.Grid", ProjectPath = "DevExpress.Xpf.Grid" });
-            items.Add(new TrackItem() { Path = @"$/2015.2/XPF/DevExpress.Xpf.NavBar", ProjectPath = "DevExpress.Xpf.NavBar" });
-            items.Add(new TrackItem() { Path = @"$/2015.2/XPF/DevExpress.Xpf.PropertyGrid", ProjectPath = "DevExpress.Xpf.PropertyGrid" });
-            items.Add(new TrackItem() { Path = @"$/2015.2/XPF/DevExpress.Xpf.Ribbon", ProjectPath = "DevExpress.Xpf.Ribbon" });
-            TrackBranch branch = new TrackBranch("2015.2", "$/2015.2/xpf_common_sync.config", "$/2015.2/XPF", items);
+            items.Add(new TrackItem() { Path = $@"$/{branchName}/XPF/DevExpress.Mvvm", ProjectPath = "DevExpress.Mvvm" });
+            items.Add(new TrackItem() { Path = $@"$/{branchName}/XPF/DevExpress.Xpf.Core", ProjectPath = "DevExpress.Xpf.Core" });
+            items.Add(new TrackItem() { Path = $@"$/{branchName}/XPF/DevExpress.Xpf.Controls", ProjectPath = "DevExpress.Xpf.Controls" });
+            items.Add(new TrackItem() { Path = $@"$/{branchName}/XPF/DevExpress.Xpf.Grid", ProjectPath = "DevExpress.Xpf.Grid" });
+            items.Add(new TrackItem() { Path = $@"$/{branchName}/XPF/DevExpress.Xpf.NavBar", ProjectPath = "DevExpress.Xpf.NavBar" });
+            items.Add(new TrackItem() { Path = $@"$/{branchName}/XPF/DevExpress.Xpf.PropertyGrid", ProjectPath = "DevExpress.Xpf.PropertyGrid" });
+            items.Add(new TrackItem() { Path = $@"$/{branchName}/XPF/DevExpress.Xpf.Ribbon", ProjectPath = "DevExpress.Xpf.Ribbon" });
+            TrackBranch branch = new TrackBranch("branchName", $@"$/{branchName}/xpf_common_sync.config", $@"$/{branchName}/XPF", items);
 
             SharpSerializerXmlSettings settings = new SharpSerializerXmlSettings();
             settings.IncludeAssemblyVersionInTypeName = false;
             settings.IncludePublicKeyTokenInTypeName = false;
             SharpSerializer serializer = new SharpSerializer(settings);
-            serializer.Serialize(new List<TrackBranch>() { branch }, @"z:\trackconfig_common.config");
+            serializer.Serialize(new List<TrackBranch>() { branch }, $@"z:\trackconfig_common_{branchName}.config");
+
         }
     }
 }
