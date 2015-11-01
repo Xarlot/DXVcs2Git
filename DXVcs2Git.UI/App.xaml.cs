66a6ada06ba4a9a11b8161edbfbb87e14f0952cb
Xarlot <maximatorr@bk.ru>
2015-11-01 13:23:34 +0300

    refactoring

--- DXVcs2Git.UI/App.xaml.cs
+++ DXVcs2Git.UI/App.xaml.cs
@@ -5,11 +5,16 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DevExpress.Xpf.Core;
 
 namespace DXVcs2Git.UI {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+            ThemeManager.ApplicationThemeName = "Office2013";
+        }
     }
 }
