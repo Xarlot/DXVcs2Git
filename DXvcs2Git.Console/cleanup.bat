75e27dfbdf281349d0d9b45f0fc25cf0cbc0442c
Xarlot <maximatorr@bk.ru>
2015-11-06 13:44:11 +0300

    cleanup

--- DXvcs2Git.Console/cleanup.bat
+++ DXvcs2Git.Console/cleanup.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set "excludeFile= cleanup.bat DXVcs2Git.Console.exe.config DXVcs2Git.UI.exe.config users.config trackconfig_common_2014.2.config trackconfig_common_2015.1.config trackconfig_common_2015.2.config trackconfig_reportdesigner.config "
-set "excludeExt= .bat .exe .txt "
+set "excludeFile= cleanup.bat DXVcs2Git.Console.exe.config DXVcs2Git.UI.exe.config users.config trackconfig_common_2014.2.config trackconfig_common_2015.1.config trackconfig_common_2015.2.config trackconfig_reportdesigner.config dxvcs2git.core.dll "
+set "excludeExt= .bat .exe .txt .dll"
 
 for %%v in (*.*) do (
    if "!excludeFile: %%v =!" equ "%excludeFile%" if "!excludeExt: %%~Xv =!" equ "%excludeExt%" del %%v
