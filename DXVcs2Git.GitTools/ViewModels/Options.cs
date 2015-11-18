fe8af47348e038069a1715bdee87adf96981ec4c
Xarlot <maximatorr@bk.ru>
2015-11-18 09:38:22 +0300

    signing

--- DXVcs2Git.GitTools/ViewModels/Options.cs
+++ DXVcs2Git.GitTools/ViewModels/Options.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DXVcs2Git.GitTools.ViewModels {
+﻿namespace DXVcs2Git.GitTools.ViewModels {
     public class Options {
         public string Token { get; set; }
 
