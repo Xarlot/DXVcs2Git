befd910246f45ca828477d4153f8ee36be0c29cc
Xarlot <maximatorr@bk.ru>
2015-11-25 13:01:11 +0300

    mvvm support

--- DXVcs2Git.UI/ViewModels/EditSelectedRepositoryViewModel.cs
+++ DXVcs2Git.UI/ViewModels/EditSelectedRepositoryViewModel.cs
@@ -9,15 +9,20 @@ namespace DXVcs2Git.UI.ViewModels {
             get { return GetProperty(() => IsInitialized); }
             private set { SetProperty(() => IsInitialized, value); }
         }
+        public BranchViewModel SelectedBranch {
+            get { return GetProperty(() => SelectedBranch); }
+            set { SetProperty(() => SelectedBranch, value); }
+        }
         protected override void OnParentViewModelChanged(object parentViewModel) {
             base.OnParentViewModelChanged(parentViewModel);
         }
 
         public void Update() {
-            
+            Refresh();
         }
         public void Refresh() {
             IsInitialized = Parent.Return(x => x.IsInitialized, () => false);
+            SelectedBranch = Parent.With(x => x.SelectedBranch);
         }
     }
 }
--- DXVcs2Git.UI/Views/ProcessMergeRequestControl.xaml.cs
+++ DXVcs2Git.UI/Views/ProcessMergeRequestControl.xaml.cs
@@ -1,12 +1,24 @@
 ﻿using System.Windows.Controls;
+using DevExpress.Mvvm;
+using DXVcs2Git.UI.ViewModels;
 
 namespace DXVcs2Git.UI {
     /// <summary>
     /// Interaction logic for ProcessMergeRequestControl.xaml
     /// </summary>
     public partial class ProcessMergeRequestControl : UserControl {
+        EditSelectedRepositoryViewModel SelectedRepository { get { return (EditSelectedRepositoryViewModel)DataContext; } }
         public ProcessMergeRequestControl() {
             InitializeComponent();
+            Messenger.Default.Register<Message>(this, OnMessage);
+        }
+        void OnMessage(Message msg) {
+            if (msg.MessageType == MessageType.Update) {
+                SelectedRepository.Update();
+            }
+            else if (msg.MessageType == MessageType.Refresh) {
+                SelectedRepository.Refresh();
+            }
         }
     }
 }
