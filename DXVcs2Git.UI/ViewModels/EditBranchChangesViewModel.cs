ff46329f902740d1107067a117633a0e373a1f13
Xarlot <maximatorr@bk.ru>
2015-11-25 19:38:34 +0300

    branch changes mvvm

---
+++ DXVcs2Git.UI/ViewModels/EditBranchChangesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.POCO;
+
+namespace DXVcs2Git.UI.ViewModels {
+    public class EditBranchChangesViewModel : ViewModelBase {
+        BranchViewModel Parent { get { return this.GetParentViewModel<BranchViewModel>(); } }
+
+        public MergeRequestViewModel MergeRequest {
+            get { return GetProperty(() => MergeRequest); }
+            private set { SetProperty(() => MergeRequest, value); }
+        }
+
+        protected override void OnParentViewModelChanged(object parentViewModel) {
+            base.OnParentViewModelChanged(parentViewModel);
+            Refresh();
+        }
+        public void Refresh() {
+            MergeRequest = Parent?.MergeRequest;
+        }
+    }
+}
