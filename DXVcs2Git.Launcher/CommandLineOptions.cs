6c35b0be752e02b3d54c0accf75b265489f8a052
Serov.Alexey <alexey.serov@devexpress.com>
2015-12-01 16:34:28 +0300

    launcher improvements

---
+++ DXVcs2Git.Launcher/CommandLineOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXVcs2Git.Launcher {
+    public class CommandLineOptions {
+        [Option('w', "wait", Default = 0, HelpText = "Wait for exit")]
+        public int WaitForExit { get; set; }
+    }
+}
