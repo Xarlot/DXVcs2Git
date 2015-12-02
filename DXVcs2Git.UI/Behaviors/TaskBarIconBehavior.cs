be2e3b4cf5940c1dd82686d7c4506413d7de590c
Serov.Alexey <alexey.serov@devexpress.com>
2015-12-02 16:47:40 +0300

    fix single-instance restoring

--- DXVcs2Git.UI/Behaviors/TaskBarIconBehavior.cs
+++ DXVcs2Git.UI/Behaviors/TaskBarIconBehavior.cs
@@ -57,8 +57,11 @@ namespace DXVcs2Git.UI.Behaviors {
         }
 
         IntPtr OnHwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled) {
-            if (msg == NativeMethods.NativeMethods.WM_SHOWGITTOOLS)
+            if (msg == NativeMethods.NativeMethods.WM_SHOWGITTOOLS) {
                 AssociatedObject.Show();
+                AssociatedObject.WindowState = WindowState.Normal;
+                AssociatedObject.Activate();                
+            }                
             return IntPtr.Zero;
         }
 
