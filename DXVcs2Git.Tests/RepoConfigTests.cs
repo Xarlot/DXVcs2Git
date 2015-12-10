86963c4be1a5f4083760fb5da4dd235d7688f8e0
Litvinov <maxim.litvinov@devexpress.com>
2015-12-10 16:19:34 +0300

    generate config tests

--- DXVcs2Git.Tests/RepoConfigTests.cs
+++ DXVcs2Git.Tests/RepoConfigTests.cs
@@ -12,14 +12,24 @@ namespace DXVcs2Git.Tests {
     [TestFixture]
     public class RepoConfigTests {
         [Test, Explicit]
-        public void GenerateRepo152Config() {
-            GenerateRepoConfig("2015.2", "TestBuild.v15.2", "XPF DXVcs2Git sync task v15.2");
+        public void GenerateCommon152Config() {
+            GenerateRepoConfig("2015.2", "TestBuild.v15.2", "XPF Common sync task v15.2", "dxvcs2git.xpf");
         }
-        void GenerateRepoConfig(string branch, string taskName, string watchTaskName) {
+        [Test, Explicit]
+        public void GenerateCommon151Config() {
+            GenerateRepoConfig("2015.1", "TestBuild.v15.1", "XPF Common sync task v15.1", "dxvcs2git.xpf");
+        }
+        [Test, Explicit]
+        public void GenerateDiagram152Config() {
+            GenerateRepoConfig("2015.2", "TestBuild.v15.2", "XPF Diagram sync task v15.2", "dxvcs2git.xpf");
+        }
+
+        void GenerateRepoConfig(string branch, string taskName, string watchTaskName, string defaultService) {
             GitRepoConfig config = new GitRepoConfig() {
                 Name = branch,
                 FarmTaskName = watchTaskName,
                 FarmSyncTaskName = taskName,
+                DefaultServiceName = defaultService,
             };
             SharpSerializerXmlSettings settings = new SharpSerializerXmlSettings();
             settings.IncludeAssemblyVersionInTypeName = false;
