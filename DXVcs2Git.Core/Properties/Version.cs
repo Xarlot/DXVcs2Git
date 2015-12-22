3fdfc8eae818226fe44c40d1435c7a1143077d89
Serov.Alexey <alexey.serov@devexpress.com>
2015-12-22 15:14:59 +0300

    2 versions before 42

--- DXVcs2Git.Core/Properties/Version.cs
+++ DXVcs2Git.Core/Properties/Version.cs
@@ -2,6 +2,6 @@
 
 namespace DXVcs2Git.Core {
     public static class VersionInfo {
-        public static readonly Version Version = new Version(1, 0, 39); // do not specify revision if 0
+        public static readonly Version Version = new Version(1, 0, 40); // do not specify revision if 0
     }
 }
