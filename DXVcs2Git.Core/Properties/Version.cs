9975aac0de8ec7c673159314b9fb23095cc1e093
Serov.Alexey <alexey.serov@devexpress.com>
2015-12-22 16:33:39 +0300

    lalalala

--- DXVcs2Git.Core/Properties/Version.cs
+++ DXVcs2Git.Core/Properties/Version.cs
@@ -2,6 +2,6 @@
 
 namespace DXVcs2Git.Core {
     public static class VersionInfo {
-        public static readonly Version Version = new Version(1, 0, 40); // do not specify revision if 0
+        public static readonly Version Version = new Version(1, 0, 41); // do not specify revision if 0
     }
 }
