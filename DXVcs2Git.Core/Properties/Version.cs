aec9c6045ecbf898d102be8ca47c36a45cc1e9dc
Litvinov <maxim.litvinov@devexpress.com>
2016-01-11 19:30:12 +0300

    add dataaccess configs

--- DXVcs2Git.Core/Properties/Version.cs
+++ DXVcs2Git.Core/Properties/Version.cs
@@ -2,6 +2,6 @@
 
 namespace DXVcs2Git.Core {
     public static class VersionInfo {
-        public static readonly Version Version = new Version(1, 0, 45); // do not specify revision if 0
+        public static readonly Version Version = new Version(1, 0, 46); // do not specify revision if 0
     }
 }
