dafc9b46359bb0ae1cc62fa8bc4bbc2334df3ca4
Litvinov <maxim.litvinov@devexpress.com>
2016-01-12 16:08:39 +0300

    improve parse login

--- DXVcs2Git.Tests/GitlabWrapperTests.cs
+++ DXVcs2Git.Tests/GitlabWrapperTests.cs
@@ -1,21 +1,23 @@
-﻿using System.Threading.Tasks;
-using DXVcs2Git.Git;
+﻿using DXVcs2Git.Git;
 using NUnit.Framework;
 
 namespace DXVcs2Git.Tests {
+    public static class TestCredentials {
+        public const string GitLabToken = "X6XV2G_ycz_U4pi4m93K";
+        public const string GitServer = "http://litvinov-lnx";
+        public const string VcsServer = @"net.tcp://vcsservice.devexpress.devx:9091/DXVCSService";
+    }
     [TestFixture]
     public class GitlabWrapperTests {
-        const string token = "X6XV2G_ycz_U4pi4m93K";
-
         [Test]
         public void GetProject() {
-            GitLabWrapper wrapper = new GitLabWrapper("http://litvinov-lnx", token);
+            GitLabWrapper wrapper = new GitLabWrapper(TestCredentials.GitServer, TestCredentials.GitLabToken);
             var project = wrapper.FindProject("tester/testxpfall");
             Assert.IsNotNull(project);
         }
         [Test]
         public void GetMergeRequests() {
-            GitLabWrapper wrapper = new GitLabWrapper("http://litvinov-lnx", token);
+            GitLabWrapper wrapper = new GitLabWrapper(TestCredentials.GitServer, TestCredentials.GitLabToken);
             var project = wrapper.FindProject("tester/testxpfall");
             var requests = wrapper.GetMergeRequests(project, x => x.TargetBranch == "test2");
         }
---
+++ DXVcs2Git.Tests/Users.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using DXVcs2Git.Core;
+using DXVcs2Git.Core.AD;
+using DXVcs2Git.DXVcs;
+using DXVcs2Git.Git;
+using NUnit.Framework;
+
+namespace DXVcs2Git.Tests {
+    [TestFixture]
+    public class Users {
+        [Test]
+        public void ParseLogin() {
+            Assert.AreEqual("test", VcsUserParser.ParseUser(@"corp\test"));
+            Assert.AreEqual("test", VcsUserParser.ParseUser(@"Corp\test"));
+            Assert.AreEqual(null, VcsUserParser.ParseUser(@"Corp\ test"));
+            Assert.AreEqual("barakhov", VcsUserParser.ParseUser(@"Corp\barakhov"));
+        }
+        [Test]
+        public void SearchADUsers() {
+            var dict = ADWrapper.GetUsers().ToDictionary(x => x.UserName);
+            var user = dict["litvinov"];
+            Assert.IsNotNull(user.Email);
+        }
+        [Test]
+        public void SearchGitLabUser() {
+            GitLabWrapper wrapper = new GitLabWrapper(TestCredentials.GitServer, TestCredentials.GitLabToken);
+            var dict = wrapper.GetUsers().ToDictionary(x => x.Username);
+            var user = dict["Litvinov"];
+            Assert.IsNotNull(user.Email);
+        }
+        [Test]
+        public void SearchRegisteredUser() {
+            GitLabWrapper gitLabWrapper = new GitLabWrapper(TestCredentials.GitServer, TestCredentials.GitLabToken);
+            DXVcsWrapper vcsWrapper = new DXVcsWrapper(TestCredentials.VcsServer);
+            RegisteredUsers users = new RegisteredUsers(gitLabWrapper, vcsWrapper);
+
+            var user = users.GetUser("litvinov");
+            Assert.IsNotNull(user);
+            Assert.IsTrue(user.IsRegistered);
+
+            var user2 = users.GetUser("Litvinov");
+            Assert.IsNotNull(user2);
+            Assert.IsTrue(user2.IsRegistered);
+        }
+        [Test, Explicit]
+        public void RegisterUser() {
+            GitLabWrapper gitLabWrapper = new GitLabWrapper(TestCredentials.GitServer, TestCredentials.GitLabToken);
+            DXVcsWrapper vcsWrapper = new DXVcsWrapper(TestCredentials.VcsServer);
+            RegisteredUsers users = new RegisteredUsers(gitLabWrapper, vcsWrapper);
+            var user = users.GetUser("Barakhov");
+        }
+    }
+}
