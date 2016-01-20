6edce988fe3e8c570fe23a6c7ea5d5d43d01a256
Litvinov <maxim.litvinov@devexpress.com>
2016-01-20 21:22:57 +0300

    base listener support

--- DXvcs2Git.Console/Program.cs
+++ DXvcs2Git.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 using System.Text;
 using CommandLine;
@@ -40,7 +41,37 @@ namespace DXVcs2Git.Console {
             return 1;
         }
         static int DoListenerWork(CommandLineOptions clo) {
+            var web = new HttpListener();
 
+            web.Prefixes.Add("http://localhost:8080/");
+
+            System.Console.WriteLine("Listening..");
+
+            web.Start();
+
+            System.Console.WriteLine(web.GetContext());
+
+            var context = web.GetContext();
+
+            var response = context.Response;
+
+            const string responseString = "<html><body>Hello world</body></html>";
+
+            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+            response.ContentLength64 = buffer.Length;
+
+            var output = response.OutputStream;
+
+            output.Write(buffer, 0, buffer.Length);
+
+            System.Console.WriteLine(output);
+
+            output.Close();
+
+            web.Stop();
+
+            System.Console.ReadKey();
             return 0;
         }
         static int DoSyncWork(CommandLineOptions clo) {
