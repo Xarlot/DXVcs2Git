1f1451d3c1b989bbc2880fc8649bf755e9e90d5e
Litvinov <maxim.litvinov@devexpress.com>
2016-02-08 17:12:28 +0300

    update configs

--- DXVcs2Git.Core/Properties/Version.cs
+++ DXVcs2Git.Core/Properties/Version.cs
@@ -2,6 +2,6 @@
 
 namespace DXVcs2Git.Core {
     public static class VersionInfo {
-        public static readonly Version Version = new Version(1, 0, 58); // do not specify revision if 0
+        public static readonly Version Version = new Version(1, 0, 59); // do not specify revision if 0
     }
 }
