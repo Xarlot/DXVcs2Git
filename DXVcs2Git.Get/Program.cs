7e42b4c1c3bd826c73fbbddf8ca7d5582139d6e7
Maxim Litvinov <maximatorr@bk.ru>
2016-07-09 09:46:47 +0300

    add log4net

--- DXVcs2Git.Get/Program.cs
+++ DXVcs2Git.Get/Program.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
+using DXVcs2Git.Core;
 
 namespace DXVcs2Git.Get {
     class Program {
         static void Main(string[] args) {
+            var result = Parser.Default.ParseArguments<CommandLineOptions>(args);
+            var exitCode = result.MapResult(clo => {
+                try {
+                    return DoWork(clo);
+                }
+                catch (Exception ex) {
+                    Log.Error("Application crashed with exception", ex);
+                    return 1;
+                }
+            },
+            errors => 1);
+            Environment.Exit(exitCode);
+        }
+
+        static int DoWork(CommandLineOptions clo) {
+            throw new NotImplementedException();
         }
     }
 }
