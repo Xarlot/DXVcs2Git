f6df7140db3f64e30a7ef294c56fb289c32adda2
Maxim Litvinov <maximatorr@bk.ru>
2016-07-11 12:09:54 +0300

    update branches control

--- DXVcs2Git.UI/Views/BranchChangesControl.xaml.cs
+++ DXVcs2Git.UI/Views/BranchChangesControl.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 using DevExpress.Mvvm;
 using DXVcs2Git.UI.ViewModels;
 
@@ -20,13 +7,8 @@ namespace DXVcs2Git.UI.Views {
     /// Interaction logic for BranchChangesControl.xaml
     /// </summary>
     public partial class BranchChangesControl : UserControl {
-        EditBranchChangesViewModel BranchChanged { get { return (EditBranchChangesViewModel)DataContext; } }
         public BranchChangesControl() {
             InitializeComponent();
-            Messenger.Default.Register<Message>(this, OnMessage);
-        }
-        void OnMessage(Message obj) {
-            BranchChanged.Refresh();
         }
     }
 }
