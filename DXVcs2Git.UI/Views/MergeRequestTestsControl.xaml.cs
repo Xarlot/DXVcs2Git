1f4d3bb187ac85c7295151a8c7fc7c4c61e41463
Maxim Litvinov <maximatorr@bk.ru>
2016-07-11 19:56:40 +0300

    add tests

---
+++ DXVcs2Git.UI/Views/MergeRequestTestsControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DXVcs2Git.UI.Views {
+    /// <summary>
+    /// Interaction logic for MergeRequestTestsControl.xaml
+    /// </summary>
+    public partial class MergeRequestTestsControl : UserControl {
+        public MergeRequestTestsControl() {
+            InitializeComponent();
+        }
+    }
+}
