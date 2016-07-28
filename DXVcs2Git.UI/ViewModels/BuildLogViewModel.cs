6aa25d21a815f33b9aeef61c519b883ea5507b92
Maxim Litvinov <maximatorr@bk.ru>
2016-07-28 12:29:35 +0300

    update

---
+++ DXVcs2Git.UI/ViewModels/BuildLogViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.Mvvm;
+
+namespace DXVcs2Git.UI.ViewModels {
+    public class BuildLogViewModel : BindableBase {
+        public BuildLogViewModel() {
+            Text = "text";
+        }
+
+        public string Text {
+            get { return GetProperty(() => Text); }
+            private set { SetProperty(() => Text, value); }
+        }
+    }
+}
--- DXVcs2Git.UI/ViewModels/EditBuildLogsViewModel.cs
+++ DXVcs2Git.UI/ViewModels/EditBuildLogsViewModel.cs
@@ -4,7 +4,12 @@ namespace DXVcs2Git.UI.ViewModels {
     public class EditBuildLogsViewModel : ViewModelBase {
         CommitViewModel Commit => (CommitViewModel)((ISupportParentViewModel)this).ParentViewModel;
         public EditBuildLogsViewModel() {
-            
+
+        }
+
+        public BuildLogViewModel Build {
+            get { return GetProperty(() => Build); }
+            private set { SetProperty(() => Build, value); }
         }
 
         protected override void OnParentViewModelChanged(object parentViewModel) {
@@ -13,6 +18,7 @@ namespace DXVcs2Git.UI.ViewModels {
         }
         void Initialize() {
             Commit.DownloadArtifacts();
+            Build = new BuildLogViewModel();
         }
     }
 }
