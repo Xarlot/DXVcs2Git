793d62f908feda135366acf4f06fd357eed7dc4a
zinovyev.petr <petr.zinovyev@devexpress.com>
2016-08-04 13:29:00 +0300

    actualize GitLabNet

--- DXVcs2Git.Core/GitLab/GitLabWrapperNet.cs
+++ DXVcs2Git.Core/GitLab/GitLabWrapperNet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using DXVcs2Git.Core;
@@ -14,8 +15,15 @@ namespace DXVcs2Git.Git {
         public GitLabWrapperNet(string server, string token) {
             client = new GitLabClient(new Uri(server), token);
         }
+        public async Task<bool> IsAdmin() {
+            var res = await client.Users.GetCurrent();
+            return await Task.FromResult<bool>(res.Data.IsAdmin.GetValueOrDefault(false));
+        }
         public async Task<PaginatedResult<Project>> GetProjects() {
             return await client.Projects.Accessible();
+        }
+        public async Task<PaginatedResult<Project>> GetAllProjects() {
+            return await client.Projects.GetAll();
         }
         public async Task<RequestResult<Project>> GetProject(uint id) {
             return await client.Projects.Find(id);
@@ -114,11 +122,18 @@ namespace DXVcs2Git.Git {
                 return await Task.FromResult<RequestResult<Comment>>(null);
             return await client.MergeRequests.CreateComment(mergeRequest.ProjectId.Value, mergeRequest.Id.Value, comment);
         }
-        public async Task<RequestResult<List<SystemHook>>> GetHooks(Project project) {
-            return await client.SystemHooks.GetAll();
+        public async Task<RequestResult<List<SystemHook>>> GetProjectsHooks(Project project) {//warning! Need testing
+            return await client.SystemHooks.GetAll(project.Id);
         }
-        public async Task<RequestResult<SystemHook>> UpdateProjectHook(Project project, SystemHook hook, Uri uri) {//warning! Need testing
-            return await client.SystemHooks.Update(hook.Id, uri.AbsolutePath, mergeRequestsEvents: true, pushEvents: true);
+        public async Task<SystemHook> FindProjectHook(Project project, Func<SystemHook, bool> projectHookHandler) {//warning! Need testing
+            var hooks = await GetProjectsHooks(project);
+            return await Task.FromResult<SystemHook>(hooks.Data.FirstOrDefault(projectHookHandler));
+        }
+        public async Task<RequestResult<SystemHook>> CreateProjectHook(Project project, Uri url, bool mergeRequestEvents, bool pushEvents, bool buildEvents) {//warning! Need testing
+            return await client.SystemHooks.Create(url.AbsolutePath, mergeRequestEvents, pushEvents, buildEvents, enableSSLVerification: false);
+        }
+        public async Task<RequestResult<SystemHook>> UpdateProjectHook(Project project, SystemHook hook, Uri uri, bool mergeRequestEvents, bool pushEvents, bool buildEvents) {//warning! Need testing
+            return await client.SystemHooks.Update(hook.Id, uri.AbsolutePath, mergeRequestEvents, pushEvents, buildEvents, enableSSLVerification: false);
         }
         public async Task<RequestResult<List<Comment>>> GetComments(MergeRequest mergeRequest) {//warning! Need testing
             if(mergeRequest.ProjectId == null || mergeRequest.Id == null)
@@ -141,6 +156,25 @@ namespace DXVcs2Git.Git {
             if(mergeRequest.ProjectId == null)
                 return await Task.FromResult<PaginatedResult<Build>>(null);
             return await client.Builds.GetByCommit(mergeRequest.ProjectId.Value, sha);
+        }
+        public async Task<RequestResult<Build>> ForceBuild(MergeRequest mergeRequest) {
+            if(mergeRequest.ProjectId == null)
+                return await Task.FromResult<RequestResult<Build>>(null);
+            foreach(var build in (await client.Builds.GetByProject(mergeRequest.ProjectId.Value, new BuildStatus[] { BuildStatus.Failed })).Data) {
+                return await client.Builds.Retry(mergeRequest.ProjectId.Value, build.Id);
+            }
+            return await Task.FromResult<RequestResult<Build>>(null);
+        }
+        public async Task<RequestResult<byte[]>> DownloadArtifacts(string projectUrl, Build build) { //need full path now (with server address)
+            var project = await IsAdmin() ? await FindProjectFromAll(projectUrl) : await FindProject(projectUrl);
+            if(project == null)
+                return await Task.FromResult<RequestResult<byte[]>>(null);
+            return await client.Builds.GetArtifacts(project.Id, build.Id);
+        }
+        public async Task<RequestResult<byte[]>> DownloadArtifacts(MergeRequest mergeRequest, Build build) {
+            if(mergeRequest.ProjectId == null)
+                return await Task.FromResult<RequestResult<byte[]>>(null);
+            return await client.Builds.GetArtifacts(mergeRequest.ProjectId.Value, build.Id);
         }
     }
 }
