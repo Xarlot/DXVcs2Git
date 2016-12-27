2116c270160a6b2e47455fab25e787bb3c5904f7
Maxim Litvinov <maximatorr@bk.ru>
2016-12-27 14:45:58 +0300

    scheduler testing

--- DXVcs2Git.Core/Properties/Version.cs
+++ DXVcs2Git.Core/Properties/Version.cs
@@ -2,7 +2,7 @@
 
 namespace DXVcs2Git.Core {
     public static class VersionInfo {
-        public const string VersionString = "1.2.49"; // do not specify revision if 0
+        public const string VersionString = "1.2.50"; // do not specify revision if 0
         public static readonly Version Version = new Version(VersionString); 
     }
 }
