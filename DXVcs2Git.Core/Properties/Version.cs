79bca568d02c430b2bec48867d149646579e5437
Maxim Litvinov <maximatorr@bk.ru>
2017-03-14 11:22:26 +0300

    add report configs

--- DXVcs2Git.Core/Properties/Version.cs
+++ DXVcs2Git.Core/Properties/Version.cs
@@ -2,7 +2,7 @@
 
 namespace DXVcs2Git.Core {
     public static class VersionInfo {
-        public const string VersionString = "1.3.5"; // do not specify revision if 0
+        public const string VersionString = "1.3.7"; // do not specify revision if 0
         public static readonly Version Version = new Version(VersionString); 
     }
 }
