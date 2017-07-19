36602c4d3d2e4f1cdbb181918c9a4366b448b8dc
Maxim Litvinov <maximatorr@bk.ru>
2017-07-19 08:34:30 +0300

    port command line

--- DXVcs2Git.WebHook/WebHook.py
+++ DXVcs2Git.WebHook/WebHook.py
@@ -12,17 +12,17 @@ def update_project_hook(project, hook, hook_addr):
     base_path = "{0.path}".format(base_url_split)
 
     if (base_path != "/{0}/".format(hook_addr)):
-        pass
+        return
 
     local_ip = socket.gethostbyname(socket.gethostname())
     if (base_url == local_ip):
-        pass
+        return
     url = list(base_url_split)
     url[1] = local_ip + ':' + base_port
     new_url = urllib.parse.urlunsplit(url)
 
-    # hook.url = new_url
-    # hook.save()
+    hook.url = new_url
+    hook.save()
     print('Project {0}. Web hook updated to {1}'.format(project.http_url_to_repo, new_url))
 
     pass
@@ -31,15 +31,25 @@ def main(argv):
     parser = OptionParser()
     parser.add_option("-w", "--webhook", default="sharedwebhook", action="store", type="string", dest="webhook")
     parser.add_option("-i", "--timeout", dest="timeout", default=30)
+    parser.add_option("-t", "--task", dest="task")
+    parser.add_option("-s", "--server", dest="server")
+    parser.add_option("-l", "--login", dest="login")
+    parser.add_option("-r", "--repo", dest="repo")
+    parser.add_option("-p", "--password", dest="password")
+    parser.add_option("-a", "--auth", dest="token")
+    parser.add_option("-u", "--update", dest="update", default=False)
 
     (options, args) = parser.parse_args()
 
-    gl = gitlab.Gitlab('http://gitserver', 'FZ8YxktKW95wsxybykxs', api_version=4)
-    projects = gl.projects.list()
-    for project in projects:
-        for hook in project.hooks.list():
-            update_project_hook(project, hook, options.webhook)
-        print(project)
+    supportsendingmessages = True if options.task else False
+
+    gl = gitlab.Gitlab(options.server, options.token, api_version=4)
+    if options.update:
+        projects = gl.projects.list(all=True)
+        for project in projects:
+            for hook in project.hooks.list():
+                update_project_hook(project, hook, options.webhook)
+            print(project)
     pass
 
 if __name__ == "__main__":
