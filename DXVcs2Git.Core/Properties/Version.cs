aacc84cc07b878ad023a461b77867f5d3a1b7b3a
Maxim Litvinov <maximatorr@bk.ru>
2017-08-01 18:22:43 +0300

    up version and add office

--- DXVcs2Git.Core/Properties/Version.cs
+++ DXVcs2Git.Core/Properties/Version.cs
@@ -2,7 +2,7 @@
 
 namespace DXVcs2Git.Core {
     public static class VersionInfo {
-        public const string VersionString = "1.3.14"; // do not specify revision if 0
+        public const string VersionString = "1.3.16"; // do not specify revision if 0
         public static readonly Version Version = new Version(VersionString); 
     }
 }
