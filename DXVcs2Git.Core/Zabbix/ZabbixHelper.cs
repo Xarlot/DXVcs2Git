492d9ba49b3bc726dce97806bbcd9d02f1dc79ca
Xarlot <maxim.litvinov@devexpress.com>
2021-04-01 09:55:20 +0300

    fx

--- DXVcs2Git.Core/Zabbix/ZabbixHelper.cs
+++ DXVcs2Git.Core/Zabbix/ZabbixHelper.cs
@@ -6,23 +6,22 @@ namespace DXVcs2Git.Core.Zabbix {
     public static class ZabbixHelper {
         public static void Send(string branch, string message) {
             try {
-                string hostName = GetHostName();
-                Log.Message($" Send info to zabbix - {hostName}.{branch}.git_checkout_time - {message}");
-                var r2 = new ZS_Request("ciserver", $"{hostName}.{branch}.git_checkout_time", message.Replace(" ", "_"));
+                Log.Message($" Send info to zabbix - {branch}.git_checkout_time - {message}");
+                var r2 = new ZS_Request("ciserver", $"{branch}.git_checkout_time", message.Replace(" ", "_"));
                 r2.Send("zabbix");
             }
             catch (Exception ex) {
                 Log.Message("Send info to zabbix failed with exception ", ex);
             }
         }
-        static string GetHostName() {
-            try {
-                using RegistryKey registryKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(@"SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters");
-                return ((string)registryKey.GetValue("HostName")).Split(new char[] {'.'})[0];
-            }
-            catch (Exception ex) {
-                return Environment.MachineName;
-            }
-        }
+        // static string GetHostName() {
+        //     try {
+        //         using RegistryKey registryKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(@"SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters");
+        //         return ((string)registryKey.GetValue("HostName")).Split(new char[] {'.'})[0];
+        //     }
+        //     catch (Exception ex) {
+        //         return Environment.MachineName;
+        //     }
+        // }
     }
 }
